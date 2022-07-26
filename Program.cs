a6bf410cf84f38b48b65a0ba5fe3bfc34ae734bc
Francesco Belacca <francesco.belacca@agic.cloud>
2022-07-26 20:00:54 +0000

    first implementation of linkedin login

--- Program.cs
+++ Program.cs
@@ -10,4 +10,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 
+builder.Services.AddOptions();
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add("r_emailaddress");
+    options.ProviderOptions.DefaultScopes.Add("r_liteprofile");
+});
+builder.Services.AddAuthorizationCore();
+
 await builder.Build().RunAsync();
