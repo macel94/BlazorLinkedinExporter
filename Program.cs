2d51e27a00ef53f1cd35ce2b48bbe8f0d58f3388
Francesco Belacca <francesco.belacca@agic.cloud>
2022-08-02 20:48:08 +0200

    Continued trying to generate client from the swagger i created

--- Program.cs
+++ Program.cs
@@ -7,5 +7,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp =>
+{
+    var dedicatedHttpClient = new HttpClient();
+    return new LinkedinClient(dedicatedHttpClient);
+});
 
 await builder.Build().RunAsync();
