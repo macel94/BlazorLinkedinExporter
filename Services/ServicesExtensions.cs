0303b670a84880f1d9352dde2e003360b9c247a6
Francesco Belacca <francesco.belacca@agic.cloud>
2022-08-05 15:16:08 +0200

    init

---
+++ Services/ServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MACEL94.github.io.Services.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MACEL94.github.io.Services
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddLinkedinAuthenticationClient(this IServiceCollection services)
+        {
+            services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+            services.AddScoped<AuthStateProvider, AuthStateProvider>();
+            return services;
+        }
+    }
+}
