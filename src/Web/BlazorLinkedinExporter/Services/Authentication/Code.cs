ad8074025d0b4ad0d73da981f1c26525cb26811f
Francesco Belacca <francesco.belacca@agic.cloud>
2022-09-04 20:55:34 +0200

    code cleanup and started error handling/refactoring

--- src/Web/BlazorLinkedinExporter/Services/Authentication/Code.cs
+++ src/Web/BlazorLinkedinExporter/Services/Authentication/Code.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using Blazored.LocalStorage;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -8,22 +9,34 @@ public class Code
 {
     public const string ClientId = "78t8sa3mzru5lu";
 
-    public static string State = "1234567890";
+    public static string State = "";
 
     public static string CodeVerifier = "";
 
     public static string CodeChallenge = "";
 
-    public static void Init()
+    public static async Task InitAsync(ILocalStorageService localStorageService)
     {
-        CodeVerifier = GenerateNonce();
+        var seed = int.Parse(DateTimeOffset.UtcNow.Ticks.ToString()[^9..]);
+        var random = new Random(seed);
+
+        CodeVerifier = GenerateNonce(random);
         CodeChallenge = GenerateCodeChallenge(CodeVerifier);
+        State = GenerateRandomState(random);
+
+        await localStorageService.SetItemAsync(nameof(CodeChallenge), CodeChallenge);
+        await localStorageService.SetItemAsync(nameof(CodeVerifier), CodeVerifier);
+        await localStorageService.SetItemAsync(nameof(State), State);
     }
 
-    private static string GenerateNonce()
+    private static string GenerateRandomState(Random random)
+    {
+        return random.Next(111111111, 999999999).ToString();
+    }
+
+    private static string GenerateNonce(Random random)
     {
         const string chars = "abcdefghijklmnopqrstuvwxyz123456789";
-        var random = new Random();
         var nonce = new char[128];
         for (int i = 0; i < nonce.Length; i++)
         {
